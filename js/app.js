863880c9ab2d5dba528412d0bb3a61cd3a521717
Helijas <gricaeshka1997@gmail.com>
2023-06-17 21:06:41 +0300

    gravity

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
-document.addEventListener('mousemove', e => {
-	Object.assign(document.documentElement, {
-		style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -0.004}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * -0.009}deg;
-		`
+if (window.screen.width >= 769){
+	document.addEventListener('mousemove', e => {
+		Object.assign(document.documentElement, {
+			style: `
+			--move-x: ${(e.clientX - window.innerWidth / 2) * -0.004}deg;
+			--move-y: ${(e.clientY - window.innerHeight / 2) * -0.009}deg;
+			`
+		})
 	})
-})
+} else if(window.screen.width <= 769){
+	if (window.DeviceMotionEvent) {
+		window.addEventListener('devicemotion', motion_hook, false);
+	}
+	else {
+		// DeviceMotionEvent не поддерживается
+	}
+	
+	// Обработчик события DeviceMotionEvent
+	function motion_hook(event) {
+		console.log('Accelerometer: '
+			+ 'X=' + event.accelerationIncludingGravity.x
+			+ 'Y=' + event.accelerationIncludingGravity.y
+			+ 'Z=' + event.accelerationIncludingGravity.z
+		);
+		Object.assign(document.documentElement, {
+			style: `
+			--move-x: ${(event.accelerationIncludingGravity.x - window.innerWidth / 2) * -0.004}deg;
+			--move-y: ${(event.accelerationIncludingGravity.y - window.innerHeight / 2) * -0.009}deg;
+			`
+		})
+	}
+}
 
